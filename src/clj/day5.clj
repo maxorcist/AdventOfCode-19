0c66a4e7436b89ce3d61f63210eb6186b70c4b9a
Max Söderquist <maxism@Maxs-MBP.lan>
2019-12-08 23:28:54 +0100

    continue with day5

--- src/clj/day5.clj
+++ src/clj/day5.clj
@@ -8,5 +8,19 @@
   (vec (map read-string (clojure.string/split input #","))))
 
 (defn add-op
-  [input addr1 addr2 addr3]
-  )
+  {:test (fn [] (is (= [5 2 3 4] (add-op [1 2 3 4] 2 3 0))))}
+  [input val1 val2 addr]
+  (assoc input addr (+ val1 val2)))
+(defn mult-op [])
+
+(defn pos-mode [x] #())
+(defn im-mode [x] x)
+
+(defn opcode
+  {:test (fn [] (is (= [02 1 2 3] (opcode 32102))))}
+  [input]
+  (let [input (map #(- (int %) 48) (reverse (format "%05d" input)))
+        [op1 op2 param1 param2 param3] input
+        op (+ op1 (* op2 10))]
+    [op param1 param2 param3]))
+
