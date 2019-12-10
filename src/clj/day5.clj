1a933659dc0119ca1eab298153dc57a23a0e19c0
Max Söderquist <max.soderquist@evry.com>
2019-12-10 17:54:56 +0100

    cont day5

--- src/clj/day5.clj
+++ src/clj/day5.clj
@@ -17,6 +17,26 @@
   {:test (fn [] (is (= [6 2 3 4] (mult-op [1 2 3 4] 2 3 0))))}
   [input val1 val2 addr]
   (assoc input addr (* val1 val2)))
+(defn jump-if-true
+  [pos val1 addr1]
+  (if (zero? val1)
+    pos
+    addr1))
+(defn jump-if-false
+  [pos val1 addr1]
+  (if (pos? val1)
+    addr1
+    pos))
+(defn less-than
+  [input val1 val2 addr]
+  (if (< val1 val2)
+    (assoc input addr 1)
+    (assoc input addr 0)))
+(defn eq
+  [input val1 val2 addr]
+  (if (= val1 val2)
+    (assoc input addr 1)
+    (assoc input addr 0)))
 
 (defn pos-mode [input addr] (get input addr))
 (defn im-mode [_ value]  value)
@@ -47,23 +67,35 @@
 
 
 (defn intcode-comp
-  {:test (fn [] (is (= [6 4 5 1 3 3 99] (intcode-comp [01 4 5 0 03 3 99])))
-           (is (= [16 4 1 0 4 0 99] (intcode-comp [102 4 1 0 04 0 99]))))}
+  {:test (fn [] (is (= 6 (intcode-comp [01 4 5 0 03 3 99])))
+           (is (= 16 (intcode-comp [102 4 1 0 04 0 99])))
+           (is (= 3 (intcode-comp [3 9 8 9 10 9 4 9 99 -1 8]))))}
   [input]
   (let [startvalue 1]
     (loop [input input
            pos 0]
-      (if (or (< 4 (mod (get input pos) 10)) (nil? (get input pos)))
-        "done"
-        (if (< 2 (mod (get input pos) 10))
-          (recur ((opcode (get input pos))
-                  input
-                  (get input (+ pos 1))
-                  startvalue)
-                 (+ pos 2))
-          (recur ((opcode (get input pos))
-                  input
-                  (get input (+ pos 1))
-                  (get input (+ pos 2))
-                  (get input (+ pos 3)))
-                 (+ pos 4)))))))
+      (condp contains? (mod (get input pos) 10)
+        #{9 nil} (do (println input) (first input))
+        #{8}
+        (recur (eq input (get input (+ pos 1)) (get input (+ pos 2)) (get input (+ pos 3)))
+         (+ pos 4))
+        #{7}
+        (recur (less-than input (get input (+ pos 1)) (get input (+ pos 2)) (get input (+ pos 3)))
+         (+ pos 4))
+        #{6}
+        (recur input (jump-if-false pos (get input (+ pos 1)) (get input (+ pos 2))))
+        #{5}
+        (recur input (jump-if-true pos (get input (+ pos 1)) (get input (+ pos 2))))
+        #{3 4}
+        (recur ((opcode (get input pos))
+                input
+                (get input (+ pos 1))
+                startvalue)
+               (+ pos 2))
+        #{1 2}
+        (recur ((opcode (get input pos))
+                input
+                (get input (+ pos 1))
+                (get input (+ pos 2))
+                (get input (+ pos 3)))
+               (+ pos 4))))))
