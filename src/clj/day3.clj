c3e8d4fb2b1615b458b6cf3eefbb102ab0f7b1cc
Max Söderquist <max.soderquist@evry.com>
2019-12-16 18:08:09 +0100

    day3

--- src/clj/day3.clj
+++ src/clj/day3.clj
@@ -4,3 +4,27 @@
 
 (def input (slurp (clojure.java.io/resource "day3")))
 
+(defn split-line [input] (clojure.string/split input #"\n"))
+(defn to-vector [input] (clojure.string/split input #",|\r"))
+
+(def splitted-input (split-line input))
+(def n1 (to-vector (first splitted-input)))
+(def n2 (to-vector (second splitted-input)))
+
+(defn eval-step
+  [move]
+  (let [direction (first move)
+        steps (read-string (reduce #(str %1 %2) "" (rest move)))]
+    (condp = direction
+      \U [0 steps]
+      \R [steps 0]
+      \D [0 (* -1 steps)]
+      \L [(* -1 steps) 0])))
+
+(defn matrix-move
+  [pos next-step]
+  (let [move (eval-step next-step)]
+    (vec (map + pos move))))
+
+(eval-step "R192")
+;; (reduce #(do (println %1 %2) (+ %1 (first (eval-step %2)))) 0 n1)
