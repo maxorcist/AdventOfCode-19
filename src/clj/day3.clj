84fc5cdafa674e85a88a933474268ce99a8d9952
Max Söderquist <maxism@Maxs-MBP.lan>
2019-12-16 23:51:08 +0100

    finish day3 part 1

--- src/clj/day3.clj
+++ src/clj/day3.clj
@@ -26,5 +26,51 @@
   (let [move (eval-step next-step)]
     (vec (map + pos move))))
 
+(defn range-of-points
+  [point1 point2]
+  (if (< point1 point2)
+    (range point1 (inc point2))
+    (range point2 (inc point1))))
+
+(defn iter-move
+  [pos move]
+  (for [x (range-of-points (first pos) (first move))
+        y (range-of-points (second pos) (second move))]
+    [x y]))
+
+(defn iter-next-step
+  [pos next-step]
+  (let [move (matrix-move pos next-step)]
+    (iter-move pos move)))
+
 (eval-step "R192")
+(defn steps-into-coords
+  [input]
+  (reduce #(conj %1 (matrix-move (last %1) %2)) [[0 0]] input))
+(def coordlist1 (steps-into-coords n1))
+(def coordlist2 (steps-into-coords n2))
 ;; (reduce #(do (println %1 %2) (+ %1 (first (eval-step %2)))) 0 n1)
+
+(defn compute-and-set
+  [coordlist]
+  (loop [coordlist coordlist
+         coord-set #{}
+         pos [0 0]]
+    (if (empty? coordlist)
+      coord-set
+      (recur (rest coordlist)
+             (into coord-set (iter-move pos (first coordlist)))
+             (first coordlist)))))
+
+(def coordset1 (compute-and-set coordlist1))
+(def coordset2 (compute-and-set coordlist2))
+(def section (clojure.set/intersection coordset1 coordset2))
+
+(defn xy-sum
+  [pos]
+  (cond
+    (neg? (first pos)) (recur [(* -1 (first pos)) (second pos)])
+    (neg? (second pos)) (recur [(first pos) (* -1 (second pos))])
+    :else (+ (first pos) (second pos))))
+
+(sort (map xy-sum section))
