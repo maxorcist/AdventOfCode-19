f91852a0d657790ade394825aa9eb17556825ec8
Max Söderquist <max.soderquist@evry.com>
2019-12-17 18:01:33 +0100

    day3 part2 finished

--- src/clj/day3.clj
+++ src/clj/day3.clj
@@ -30,7 +30,7 @@
   [point1 point2]
   (if (< point1 point2)
     (range point1 (inc point2))
-    (range point2 (inc point1))))
+    (reverse (range point2 (inc point1)))))
 
 (defn iter-move
   [pos move]
@@ -43,15 +43,14 @@
   (let [move (matrix-move pos next-step)]
     (iter-move pos move)))
 
-(eval-step "R192")
 (defn steps-into-coords
   [input]
   (reduce #(conj %1 (matrix-move (last %1) %2)) [[0 0]] input))
 (def coordlist1 (steps-into-coords n1))
 (def coordlist2 (steps-into-coords n2))
-;; (reduce #(do (println %1 %2) (+ %1 (first (eval-step %2)))) 0 n1)
 
 (defn compute-and-set
+  {:test (fn [] (is (= #{[0 0] [1 0] [2 0]} (compute-and-set [[0 0] [2 0] [0 0]]))))}
   [coordlist]
   (loop [coordlist coordlist
          coord-set #{}
@@ -73,4 +72,40 @@
     (neg? (second pos)) (recur [(first pos) (* -1 (second pos))])
     :else (+ (first pos) (second pos))))
 
-(sort (map xy-sum section))
+(def part-one-result (second (sort (map xy-sum section))))
+
+(defn compute-til-stop
+  {:test (fn [] (is (= 5 (compute-til-stop [[0 0] [10 0]] [5 0])))
+           (is (= 7 (compute-til-stop [[0 0] [-10 0]] [-7 0])))
+           (is (= 120 (compute-til-stop [[0 0] [100 0] [100 -50]] [100 -20])))
+           (is (= 12 (compute-til-stop [[10 10] [10 20] [20 20]] [12 20]))))}
+  [coordlist stop]
+  (loop [coordlist coordlist
+         counter 0]
+    (let [iter-moves (iter-move (first coordlist) (second coordlist))]
+      ;; (println (first coordlist) counter iter-moves)
+      (if (or (empty? coordlist)
+              (.contains iter-moves stop))
+                  (+ counter (.indexOf iter-moves stop))
+                  (recur (next coordlist)
+                         (+ counter (dec (count iter-moves))))))))
+
+(def testdata1 "R75,D30,R83,U83,L12,D49,R71,U7,L72")
+(def testdata2  "U62,R66,U55,R34,D71,R55,D58,R83")
+(def cdata1 (steps-into-coords (to-vector testdata1)))
+(def cdata2 (steps-into-coords (to-vector testdata2)))
+(def sectdata (clojure.set/intersection (compute-and-set cdata1) (compute-and-set cdata2)))
+(defn compute-intersections
+  {:test (fn [] (is (= 610
+                       (second (sort (compute-intersections cdata1 cdata2 sectdata))))))}
+  [coordlist1 coordlist2 section]
+  (second (sort (reduce #(conj %1 (+ (compute-til-stop coordlist1 %2)
+                         (compute-til-stop coordlist2 %2))) '() section))))
+
+(defn parse-and-compute
+  [text1 text2]
+  (let [c1 (compute-and-set (steps-into-coords (to-vector text1)))
+        c2 (compute-and-set (steps-into-coords (to-vector text2)))]
+    (println (clojure.set/intersection c1 c2))
+    (compute-intersections c1 c2 (clojure.set/intersection c1 c2)
+    )))
