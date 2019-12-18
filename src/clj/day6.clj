9289304912d73e4a68dc7f6dfc2483157bb82baf
Max Söderquist <max.soderquist@evry.com>
2019-12-18 13:51:25 +0100

    finish day6

--- src/clj/day6.clj
+++ src/clj/day6.clj
@@ -4,9 +4,10 @@
 
 (def input (clojure.string/split
             (slurp (clojure.java.io/resource "day6"))
-            #"\n"))
+            #"\r\n|\n"))
 (def orbiters (map #(subs % 4) input))
-(def orbitmap (reduce #(assoc %1 (subs %2 4) (subs %2 0 3)) {} input))
+(defn map-orbits [input] (reduce #(assoc %1 (subs %2 4) (subs %2 0 3)) {} input))
+(def orbitmap (map-orbits input))
 
 (defn orbited-by
   [orbiter orbitmap]
@@ -20,3 +21,32 @@
       orbits
       (recur (orbited-by orbiter orbitmap)
              (conj orbits orbiter)))))
+(defn count-orbits
+  [orbitmap orbiter]
+  (dec (count (get-orbits orbitmap orbiter))))
+(defn orbits-from-to
+  [from to orbitmap]
+  (loop [from from
+         orbits '()]
+    (if (= to (first orbits))
+      orbits
+      (recur (orbited-by from orbitmap) (conj orbits from)))))
+
+(def small (get-orbits orbitmap "5HT"))
+
+(defn easy-iter
+  [orbitmap orbiters]
+  (loop [result 0
+         orbiters orbiters]
+    (if (empty? orbiters)
+      result
+      (recur (+ result (count-orbits orbitmap (first orbiters)))
+             (rest orbiters)))))
+
+(defn steps-to-santa
+  [orbitmap from to]
+  (let [from-you (set (get-orbits orbitmap from))
+        from-san (set (get-orbits orbitmap to))
+        section (clojure.set/intersection from-you from-san)]
+    (map #(+ -2 (dec (count (orbits-from-to from % orbitmap)))
+            (dec (count (orbits-from-to to % orbitmap)))) section)))
